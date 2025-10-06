18a6f8f59e9fbeac0d702d15c42036662d2340d2
Sakib <sakibahamedantor@gmail.com>
2025-10-06 11:32:53 +0000

    First Lesson Practice, Add 5 years

---
+++ firstlessonpractice.py
@@ -0,0 +1,10 @@
+#ask user for their name
+name=input("What is your name? ")
+
+#ask user to imput their age
+age=int(input("How Old are you? "))
+
+in_5_years= age + 5
+
+#final Output
+print(f"Hello {name}, in 5 years you will be {in_5_years} old")
