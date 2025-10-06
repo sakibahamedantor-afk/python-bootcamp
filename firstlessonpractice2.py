a2ee1cf270aa652ac85af61e6ce17502894ef63e
Sakib <sakibahamedantor@gmail.com>
2025-10-06 11:45:34 +0000

    Practice 2, Add 10 years

---
+++ firstlessonpractice2.py
@@ -0,0 +1,11 @@
+#use input their name
+name=input("Enter your name: ")
+
+#user input their age
+age=int(input ("Enter your age: "))
+
+#add 10 years
+future=age+10
+
+#output
+print(f"Hello {name}, In 10 years you will be {future} years old")
