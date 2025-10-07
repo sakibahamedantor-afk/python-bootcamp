7de512a17a77109df113a92e601e5bb589330922
Sakib <sakibahamedantor@gmail.com>
2025-10-07 07:11:43 +0000

    Conditional statement learned

---
+++ vote.py
@@ -0,0 +1,9 @@
+#taking input of age as number
+age=int(input("What is your age? :"))
+              
+
+#function
+if age>=18:
+    print(f"You are eligible to Vote!!")
+else:
+    print(f"You are not eligible to Vote!!")
