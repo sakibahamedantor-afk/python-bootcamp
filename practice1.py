90752f99af28a4eb2671e71755534b2c04a57c09
Sakib <sakibahamedantor@gmail.com>
2025-10-07 06:55:46 +0000

    Number + - * /

---
+++ practice1.py
@@ -0,0 +1,15 @@
+#takes input
+
+first_number=int(input("Enter first Number: "))
+second_number=int(input("Enter Second Number: "))
+
+#Function
+sum=first_number+second_number
+difference=first_number-second_number
+product=first_number*second_number
+division=first_number/second_number
+
+print(f"Sum is {sum}")
+print(f"Difference is {difference}")
+print(f"Multiplication is {product}")
+print(f"Division is {division}")
