754024fae601daf15416ef8e804ae535d34395df
Sakib <sakibahamedantor@gmail.com>
2025-10-09 09:21:00 +0000

    Create a list of 5 numbers (your choice)

---
+++ list.py
@@ -0,0 +1,12 @@
+#Create a list of 5 numbers (your choice).
+
+numbers=[21, 24, 14, 58, 2]
+
+numbers.append(10)
+print(numbers)
+numbers.remove(24)
+print(numbers)
+numbers.sort()
+print(numbers)
+numbers.reverse()
+print(numbers)
